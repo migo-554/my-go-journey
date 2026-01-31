db80a86e73766c20c2490fcb882c212447543c45
Michael Sharov <Mihailsarov554@gmail.com>
2026-01-31 08:37:46 +0700

    Added logging to file and time tracking

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+)
 
 func Proverka(err error) string {
 	if err != nil {
@@ -14,9 +18,11 @@ func Proverka(err error) string {
 }
 
 func main() {
+	now := time.Now()
 	for {
+		fmt.Printf("Сегодняшнее время в кемерово: %s\n", now.Format("15:04:03"))
 		var stress int
-		fmt.Println("Введите свой уровень стресса от 1 до 10:")
+		fmt.Print("Введите свой уровень стресса от 1 до 10:")
 		_, err := fmt.Scan(&stress)
 		StatStress := Proverka(err)
 		if StatStress == "not" {
@@ -24,7 +30,7 @@ func main() {
 		}
 
 		var words int
-		fmt.Println("Теперь введите колличество выученных сегодня слов на английском")
+		fmt.Print("Теперь введите колличество выученных сегодня слов на английском:")
 		_, err1 := fmt.Scan(&words)
 		StatWords := Proverka(err1)
 		if StatWords == "not" {
@@ -32,7 +38,7 @@ func main() {
 		}
 
 		var watchingYoutube int
-		fmt.Println("Введите колличество минут проебанных за ютубом")
+		fmt.Print("Введите колличество минут проебанных сегодня за ютубом:")
 		_, err2 := fmt.Scan(&watchingYoutube)
 		StatYoutube := Proverka(err2)
 		if StatYoutube == "not" {
@@ -45,8 +51,25 @@ func main() {
 		} else if words > 10 && watchingYoutube == 0 {
 			fmt.Println("Корабль идет на гиперскорости")
 		} else {
-			fmt.Printf("Системы стабильный продолжай в том же духе")
+			fmt.Println("Системы стабильный продолжай в том же духе")
 		}
+
+		f, err := os.OpenFile("log.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			fmt.Println("Ошибка доступа к журналу ", err)
+			return
+		}
+		defer f.Close()
+
+		report := fmt.Sprintf("[%s] Стресс: %d, Слов %d\n ", time.Now().Format("01.02.2006"), stress, words)
+		_, err = f.WriteString(report)
+		if err != nil {
+			fmt.Println("Ошибка с записью ", err)
+			return
+		} else {
+			fmt.Println("Данные успешно записались!")
+		}
+
 		break
 
 	}
